555b09bd89972031d98757f077684fb27483ee82
waras47 <saudinwahyudin@gmail.com>
2026-04-01 12:03:49 +0700

    Feat::add doctype mutasi barang

--- silverplast/hooks.py
+++ silverplast/hooks.py
@@ -146,6 +146,9 @@ doc_events = {
     },
     "QCE": {
         "on_update": "silverplast.api.qc.on_qc_update"
+    },
+    "Mutasi Barang": {
+        "on_submit": "silverplast.api.mutasi_barang.on_submit_hook"
     }
 }
 
